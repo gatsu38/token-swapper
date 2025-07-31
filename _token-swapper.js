2317330654c3cab168e2f2c0fca301e4e76437e9
root <root@kali.nando>
2025-07-31 16:36:16 +0200

    Added sendingRequest()

--- _token-swapper.js
+++ _token-swapper.js
@@ -30,18 +30,18 @@ function responseReceived(msg, initiator, helper) {
     }
 }
 
-function responseReceived(msg, initiator, helper) {
-    var body = msg.getResponseBody().toString();
+function sendingRequest(msg, initiator, helper) {
+    if (!global._token) return;
 
-    if (!msg.getResponseHeader().isHtml()) return;
+    if (msg.getRequestHeader().getMethod().equalsIgnoreCase("POST")) {
+        var body = msg.getRequestBody().toString();
 
-    var matcher = tokenPattern.matcher(body);
-    if (matcher.find()) {
-        var token = matcher.group(1);
-        if (token !== global._token) {
-            print("[+] Updated _token: " + token);
-            global._token = token;
+        if (body.indexOf("_token=") >= 0) {
+            // Replace _token parameter value with the global one
+            var newBody = body.replace(/_token=[^&]*/g, "_token=" + URLEncoder.encode(global._token, "UTF-8"));
+            msg.setRequestBody(newBody);
+            msg.getRequestHeader().setContentLength(newBody.length());
+            print("[*] Injected updated _token into POST: " + global._token);
         }
     }
 }
-
